a2d0337dcc6dd4f1bcd67226898cc660938ea19f
Robert Pathy <robert.pathy@vidyard.com>
2019-11-29 10:27:02 -0500

    Half working avoid mode

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import json
 import os
 import random
 import bottle
+import copy
 
 from api import ping_response, start_response, move_response, end_response
 
@@ -32,7 +33,7 @@ def ping():
 
 @bottle.post('/start')
 def start():
-    global horizontalDirection, verticalDirection, lastMove
+    global horizontalDirection, verticalDirection, lastMove, occupiedZones
     horizontalDirection = 'right'
     verticalDirection = 'up'
 
@@ -47,14 +48,47 @@ def start():
 
 @bottle.post('/move')
 def move():
-    global horizontalDirection, verticalDirection, lastMove
+    global horizontalDirection, verticalDirection, lastMove, occupiedZones
     data = bottle.request.json
 
-    head = data["you"]["body"][0]
-    body = data["you"]["body"][1]
+    head = copy.deepcopy(data["you"]["body"][0])
+    body = copy.deepcopy(data["you"]["body"][1])
+    occupiedZones = copy.deepcopy(data["you"]["body"])
 
-    print(move)
+    print(head)
 
+    if head["y"] == 1:
+        verticalDirection = 'down'
+
+    if head["y"] == 13:
+        verticalDirection = 'up'
+
+    if head["x"] == 1:
+        horizontalDirection = 'right'
+
+    if head["x"] == 13:
+        horizontalDirection = 'left'
+
+
+    if head["y"] == 1:
+        if lastMove == 'right' or lastMove == 'left':
+            move = verticalDirection
+        else:
+            move = horizontalDirection
+    elif head["y"] == 13:
+        if lastMove == 'right' or lastMove == 'left':
+            move = verticalDirection
+        else:
+            move = horizontalDirection
+    else:
+        move = verticalDirection
+
+    if not is_safe(projected_location(copy.deepcopy(head), move)):
+        print("Not Safe " + move)
+        move = find_safe(copy.deepcopy(head))
+        print("Safe Move " + move)
+
+    print(head)
     lastMove = move
     return move_response(move)
 
@@ -70,6 +104,39 @@ def end():
     print(json.dumps(data))
 
     return end_response()
+
+
+def is_safe(location):
+    global occupiedZones
+    return not location in occupiedZones
+
+
+def find_safe(head):
+    if is_safe(projected_location(copy.deepcopy(head), "left")):
+        direction = "left"
+    elif is_safe(projected_location(copy.deepcopy(head), "right")):
+        direction = "right"
+    elif is_safe(projected_location(copy.deepcopy(head), "up")):
+        direction = "up"
+    else:
+        direction = "down"
+
+    print(direction)
+    return direction
+
+
+def projected_location(head, direction):
+    if direction == "up":
+        head["y"] = head["y"] - 1
+    elif direction == "down":
+        head["y"] = head["y"] + 1
+    elif direction == "right":
+        head["x"] = head["x"] - 1
+    elif direction == "left":
+        head["x"] = head["x"] + 1
+
+    return head
+
 
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
